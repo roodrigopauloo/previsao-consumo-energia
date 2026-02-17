4a568349fc18f79b5881d2c58193de8cea204e1d
Rodrigo <rodrigo.paulo.oliveira@ccc.ufcg.edu.br>
2026-02-17 17:13:08 -0300

    feat(classification): implement mlp training

--- src/classification_model.py
+++ src/classification_model.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from loader_data import load_csv
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report
 
 
 def preparar_dados_classificacao(df):
@@ -22,8 +25,28 @@ def preparar_dados_classificacao(df):
     return df, None
 
 
+def treinar_classificador(X_train, y_train):
+    
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    
+    modelo = MLPClassifier(
+        hidden_layer_sizes=(50, 50), 
+        activation='relu',           
+        solver='adam',              
+        max_iter=2000,               
+        random_state=42              
+    )
+
+    modelo.fit(X_train_scaled, y_train)
+    
+    return modelo, scaler
+
+
 if __name__ == "__main__":
     df = load_csv()
     X, y = preparar_dados_classificacao(df)
-    print(f"Tamanho de X: {X.shape}")
-    print(f"Classes criadas:\n{y.value_counts()}")
+
+    modelo, scaler = treinar_classificador(X, y)
+    print("Modelo treinado com sucesso!")
+    print(f"Número de iterações: {modelo.n_iter_}")
